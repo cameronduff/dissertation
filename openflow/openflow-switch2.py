cf837d792e793426d5f3726d9a51def477735298
Cameron Duff <cameronsduff@gmail.com>
2024-02-21 12:17:55 +0000

    Adding comments and refactoring

--- openflow/openflow-switch2.py
+++ openflow/openflow-switch2.py
@@ -1,23 +1,3 @@
-#
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation;
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# Author: Blake Hurd <naimorai@gmail.com>
-# Modified by: Josh Pelkey <joshpelkey@gmail.com>
-#              Gabriel Ferreira <gabrielcarvfer@gmail.com>
-#
-
 # Goal:
 #      CSMA      CSMA      CSMA
 #   n0 --- OFSW0 --- OFSW1 --- n1
@@ -26,15 +6,20 @@
 
 from ns import ns
 
-ns.LogComponentEnable("OpenFlowInterface", ns.LOG_LEVEL_ALL)
-ns.LogComponentEnable("OpenFlowSwitchNetDevice", ns.LOG_LEVEL_ALL)
+#ns.LogComponentEnable("OpenFlowInterface", ns.LOG_LEVEL_ALL)
+#ns.LogComponentEnable("OpenFlowSwitchNetDevice", ns.LOG_LEVEL_ALL)
 
 print("Creating nodes")
 terminals = ns.NodeContainer()
 terminals.Create(2)
+print("Nodes created")
+print("")
 
+print("Creating switches")
 switches = ns.NodeContainer()
 switches.Create(2)
+print("Switches created")
+print("")
 
 csmaHelper = ns.CsmaHelper()
 csmaHelper.SetChannelAttribute("DataRate", ns.DataRateValue(5000000))
@@ -45,46 +30,56 @@ terminalDevices = ns.NetDeviceContainer()
 switchDevices0 = ns.NetDeviceContainer()
 switchDevices1 = ns.NetDeviceContainer()
 
-#connect n0 to ofsw0
+print("Connecting n0 --- sw0")
 container0 = ns.NodeContainer()
 container0.Add(terminals.Get(0))
 container0.Add(switches.Get(0))
-link = csmaHelper.Install(container0)
-terminalDevices.Add(link.Get(0))
-switchDevices0.Add(link.Get(1))
+link0 = csmaHelper.Install(container0)
+terminalDevices.Add(link0.Get(0))
+switchDevices0.Add(link0.Get(1))
+print('Link between terminal 0 {} and switch 0 {} created'.format(terminals.Get(0), switches.Get(0)))
+print('')
 
-#connect n1 to ofsw1
+print("Connecting sw0 --- sw1")
+container2 = ns.NodeContainer()
+container2.Add(switches.Get(0))
+container2.Add(switches.Get(1))
+link2 = csmaHelper.Install(container2)
+switchDevices0.Add(link2.Get(0))
+switchDevices1.Add(link2.Get(1))
+print('Link between switch 0 {} and switch 1 {} created'.format(switches.Get(0), switches.Get(1)))
+print('')
+
+print("Connecting sw1 --- n1")
 container1 = ns.NodeContainer()
-container1.Add(terminals.Get(0))
-container1.Add(switches.Get(0))
-link = csmaHelper.Install(container1)
-terminalDevices.Add(link.Get(0))
-switchDevices1.Add(link.Get(1))
+container1.Add(terminals.Get(1))
+container1.Add(switches.Get(1))
+link1 = csmaHelper.Install(container1)
+terminalDevices.Add(link1.Get(0))
+switchDevices1.Add(link1.Get(1))
+print('Link between switch 1 {} and terminal 1 {} created'.format(switches.Get(1), terminals.Get(1)))
+print('')
 
-#connect ofsw0 to ofsw1
-container3 = ns.NodeContainer()
-container3.Add(switches.Get(0))
-container3.Add(switches.Get(1))
-link = csmaHelper.Install(container1)
-switchDevices0.Add(link.Get(0))
-switchDevices1.Add(link.Get(1))
-
-#populate routing tables
+print("Populate routing tables")
 ipv4GlobalRoutingHelper = ns.Ipv4GlobalRoutingHelper()
 ipv4GlobalRoutingHelper.PopulateRoutingTables()
+print("Routing tables populated")
+print("")
 
-#create swicth netdevices
+#create switch netdevices
 switch0 = switches.Get(0)
 switch1 = switches.Get(1)
 openFlowSwitchHelper = ns.OpenFlowSwitchHelper()
 
-#install controller0 for ofsw0
+print("Install controller0 for sw0")
 controller0 = ns.ofi.DropController()
 openFlowSwitchHelper.Install(switch0, switchDevices0, controller0)
 
-#install controller1 for ofsw1
+print("Install controller1 for sw1")
 controller1 = ns.ofi.DropController()
 openFlowSwitchHelper.Install(switch1, switchDevices1, controller1)
+print("Drop controller installed on switch 0 and 1")
+print("")
 
 print("Installing Internet Protocol")
 internetStackHelper = ns.InternetStackHelper()
@@ -93,11 +88,26 @@ internetStackHelper.Install(terminals)
 ipv4AddressHelper = ns.Ipv4AddressHelper()
 ipv4AddressHelper.SetBase("10.1.1.0", "255.255.255.0")
 addresses = ipv4AddressHelper.Assign(terminalDevices)
+print("IP Installed")
+print("")
 
-for i in range(2):
-    print(addresses.GetAddress(i))
-    print(addresses.GetAddress(i).ConvertTo())
+for i in range(terminals.GetN()):
+    node = terminals.Get(i)
+    print('Terminal node {} has {} NetDevices'.format(i, node.GetNDevices()))
+    for j in range(node.GetNDevices()):
+        device = node.GetDevice(j)
+        print('      Node {}s NetDevice {} is {}'.format(i, j, device.GetAddress()))
+    print('')
 
+for i in range(switches.GetN()):
+    node = switches.Get(i)
+    print('Switch node {} has {} NetDevices'.format(i, node.GetNDevices()))
+    for j in range(node.GetNDevices()):
+        device = node.GetDevice(j)
+        print('      Node {}s NetDevice {} is {}'.format(i, j, device.GetAddress()))
+    print('')
+
+"""
 #sink port
 port = 9
 
@@ -116,6 +126,32 @@ app = sink.Install(terminals.Get(1))
 app.Start(ns.Seconds(0.0))
 
 #onoff.SetAttribute("Remote", ns.AddressValue(ns.InetSocketAddress(ns.Ipv4Address("10.1.1.1"), port).ConvertTo()))
+"""
+
+print("Creating destination socket")
+destinationSocket = ns.Socket.CreateSocket(terminals.Get(1), ns.UdpSocketFactory.GetTypeId())
+destinationPort = 9
+destinationAddress = ns.network.Ipv4Address("10.1.1.2")
+destinationLocalAddress = ns.InetSocketAddress(destinationAddress, destinationPort)
+destinationSocket.Bind(destinationLocalAddress.ConvertTo())
+#destinationSocket.SetRecvCallback()
+
+print("Creating source socket")
+sourceSocket = ns.Socket.CreateSocket(terminals.Get(0), ns.UdpSocketFactory.GetTypeId())
+sourceSocket.Bind()
+sourceSocket.BindToNetDevice(terminalDevices.Get(0))
+#sourceSocket.SetRecvCallback()
+print("Sockets created")
+print("")
+
+ascii = ns.network.AsciiTraceHelper()
+csmaHelper.EnableAsciiAll(ascii.CreateFileStream("openflow-switch.tr"))
+csmaHelper.EnablePcapAll("openflow-switch", False)
+
+print("Sending packet")
+#sending packet
+packet = ns.Packet(1024)
+sourceSocket.SendTo(packet, 0, destinationAddress.ConvertTo())
 
 print("Creating animation")
 #creates animation
@@ -135,9 +171,6 @@ for switch in range(switches.GetN()):
     anim.UpdateNodeDescription(terminals.GetN() + switch, "SW" + str(switch))
     anim.UpdateNodeSize(terminals.GetN() + switch, 3, 3)
 
-ascii = ns.network.AsciiTraceHelper()
-csmaHelper.EnableAsciiAll(ascii.CreateFileStream("openflow-switch.tr"))
-csmaHelper.EnablePcapAll("openflow-switch", False)
-
 ns.Simulator.Run()
 ns.Simulator.Destroy()
+
