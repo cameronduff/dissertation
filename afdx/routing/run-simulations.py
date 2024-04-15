a638009ffb5c9e8030ba704f9a05ecb4dc8084b4
Cameron Duff <cameronsduff@gmail.com>
2024-04-15 16:06:53 +0100

    automating all 3

--- afdx/routing/run-simulations.py
+++ afdx/routing/run-simulations.py
@@ -1,4 +1,16 @@
 import os
+
+#run small
 os.system('cd')
 os.system('cd ns-allinone-3.40/ns-3.40')
 os.system('./ns3 run scratch/dissertation/afdx/routing/simulations/small/afdx-network-small.cc')
+
+#run medium
+os.system('cd')
+os.system('cd ns-allinone-3.40/ns-3.40')
+os.system('./ns3 run scratch/dissertation/afdx/routing/simulations/medium/afdx-network-medium.cc')
+
+#run large
+os.system('cd')
+os.system('cd ns-allinone-3.40/ns-3.40')
+os.system('./ns3 run scratch/dissertation/afdx/routing/simulations/large/afdx-network-large.cc')
