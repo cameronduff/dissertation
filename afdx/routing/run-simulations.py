9e5150670169a646fd9aec73c8cf393912cfcd6a
Cameron Duff <cameronsduff@gmail.com>
2024-04-16 14:40:58 +0100

    updating logic to include commandline args

--- afdx/routing/run-simulations.py
+++ afdx/routing/run-simulations.py
@@ -1,16 +1,19 @@
 import os
 
-#run small
-os.system('cd')
-os.system('cd ns-allinone-3.40/ns-3.40')
-os.system('./ns3 run scratch/dissertation/afdx/routing/simulations/small/afdx-network-small.cc')
+numOfSimulations = 100
 
-#run medium
-os.system('cd')
-os.system('cd ns-allinone-3.40/ns-3.40')
-os.system('./ns3 run scratch/dissertation/afdx/routing/simulations/medium/afdx-network-medium.cc')
+for i in range(numOfSimulations):
+    #run small
+    os.system('cd')
+    os.system('cd ns-allinone-3.40/ns-3.40')
+    os.system(f'./ns3 run scratch/dissertation/afdx/routing/simulations/small/afdx-network-small.cc -- --flowmonName="afdx-metrics-small-{i+1}.xml"')
 
-#run large
-os.system('cd')
-os.system('cd ns-allinone-3.40/ns-3.40')
-os.system('./ns3 run scratch/dissertation/afdx/routing/simulations/large/afdx-network-large.cc')
+    #run medium
+    os.system('cd')
+    os.system('cd ns-allinone-3.40/ns-3.40')
+    os.system(f'./ns3 run scratch/dissertation/afdx/routing/simulations/medium/afdx-network-medium.cc -- --flowmonName="afdx-metrics-medium-{i+1}.xml"')
+
+    #run large
+    os.system('cd')
+    os.system('cd ns-allinone-3.40/ns-3.40')
+    os.system(f'./ns3 run scratch/dissertation/afdx/routing/simulations/large/afdx-network-large.cc -- --flowmonName="afdx-metrics-large-{i+1}.xml"')
